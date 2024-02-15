e707835cb6ddab5c336da9b482ccbae7614bf8f3
Nurislam <98372q@gmail.com>
2024-02-15 14:18:59 +0300

    home_work

---
+++ script.js
@@ -0,0 +1,29 @@
+alert("Кутман таң");
+
+alert("Nurislam");
+
+let myNumber = 10;
+
+myNumber = 5;
+
+document.body.innerHTML = "Кутман тан";
+
+console.log(Math.ceil(64.1));
+
+console.log(Math.floor(181.9));
+
+console.log(Math.trunc(39.32));
+
+var summa = 18.6 + 195.3;
+
+var okrug = Math.round(213, 9);
+
+console.log(Math.round(213.9));
+
+
+function generateRandomNumber() {
+	const randomNumber = Math.floor(Math.random() * 11); 
+	console.log(randomNumber); 
+}
+
+generateRandomNumber();
